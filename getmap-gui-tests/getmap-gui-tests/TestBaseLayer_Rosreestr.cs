acc2227578dc4a5d824e5465128ecfb0e9a1b576
darya <gis5@sovzond.ru>
2016-02-05 13:03:57 +0300

    Добавила проверку

--- getmap-gui-tests/getmap-gui-tests/TestBaseLayer_Rosreestr.cs
+++ getmap-gui-tests/getmap-gui-tests/TestBaseLayer_Rosreestr.cs
@@ -12,12 +12,12 @@ namespace GetMapTest
     {
         private IWebDriver driver;       
         [TestMethod]
-        public void TheXTest()
+        public void TestRosreestr()
         {
             driver = Settings.Instance.createDriver();
             IJavaScriptExecutor js = driver as IJavaScriptExecutor;
             GUI.Login.loginAsGuest(driver, Settings.Instance.BaseUrl);
-            driver.FindElement(By.Id("sovzond_widget_SimpleButton_104")).Click();
+            driver.FindElement(By.Id("sovzond_widget_SimpleButton_74")).Click();
             Thread.Sleep(5000);
             IWebElement element = driver.FindElement(By.Id("sovzond_widget_SimpleButton_0"));
             var builder = new Actions(driver);
@@ -28,16 +28,13 @@ namespace GetMapTest
                 if (el[0].Text != "Google") Assert.Fail("не найден Google");
                 if (el[4].Text != "Росреестр") Assert.Fail("не найден Росреестр");
                 if (el[5].Text != "OpenStreetMap") Assert.Fail("не найден OpenStreetMap");
-                if (el[6].Text != "Топооснова") Assert.Fail("не найден Топооснова");
             }
                 IWebElement element1 = driver.FindElement(By.Id("dijit_form_RadioButton_3"));
                 builder.Click(element1).Perform();
                
                Thread.Sleep(5000);  
-               string h= (string)js.ExecuteScript("return window.portal.stdmap.map.baseLayer.div.id.toString()");
-
-
-            
+               string h= (string)js.ExecuteScript("return window.portal.stdmap.map.baseLayer.url.toString()");
+               if( h.StartsWith("http://maps.rosreestr.ru/")!= true) Assert.Fail("не отображен базовый слой Росреестр");
         }
     }
 }
