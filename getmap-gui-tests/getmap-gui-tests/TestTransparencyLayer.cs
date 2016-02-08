3c836c6463ff6365df051a8f26c3c412fe18bdc8
pavel <gis5@sovzond.ru>
2016-02-08 14:21:28 +0300

    пустая фиксация

--- getmap-gui-tests/getmap-gui-tests/TestTransparencyLayer.cs
+++ getmap-gui-tests/getmap-gui-tests/TestTransparencyLayer.cs
@@ -27,7 +27,7 @@ namespace GetMapTest
         [TestInitialize]
         public void Setup()
         {
-            driver = Settings.Instance.createDriver();
+            driver = Settings.Instance.createDriver();        
         }
 
         /// <summary>
@@ -70,64 +70,6 @@ namespace GetMapTest
             IWebElement element = driver.FindElement(By.CssSelector(locationMap));
             var cutArea = new Rectangle(element.Location, element.Size);
             return bitmapScreen.Clone(cutArea, bitmapScreen.PixelFormat);
-        }
-        private unsafe Bitmap EqualImage(Bitmap image1,Bitmap image2,Color color)
-        {
-            if (image1 == null || image2 == null)
-                return null;
-            if (image1.Width != image2.Width || image1.Height != image2.Height)
-                return null;
-            Bitmap diffImage = image2.Clone() as Bitmap;
-            int height = image1.Height;
-            int width = image1.Width;
-            BitmapData data1 = image1.LockBits(new Rectangle(0, 0, width, height),
-                ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-            BitmapData data2 = image2.LockBits(new Rectangle(0, 0, width, height),
-                ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-            BitmapData diffData = diffImage.LockBits(new Rectangle(0, 0, width, height),
-         ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
-            byte* data1Ptr = (byte*)data1.Scan0;
-            byte* data2Ptr = (byte*)data2.Scan0;
-            byte* diffPtr = (byte*)diffData.Scan0;
-            byte[] swapColor = new byte[3];
-            swapColor[0] = color.R;
-            swapColor[1] = color.G;
-            swapColor[2] = color.B;
-            int rowPadding = data1.Stride - (image1.Width * 3);
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    int same = 0;
-                    byte[] tmp = new byte[3];
-                    for (int x = 0; x < 3; x++)
-                    {
-                        tmp[x] = data2Ptr[0];
-                        if (data1Ptr[0] == data2Ptr[0])
-                        {
-                            same++;
-                        }
-                        data1Ptr++;
-                        data2Ptr++;
-                        for (int y = 0; y < 3; y++)
-                        {
-                            diffPtr[0] = (same == 3) ? swapColor[x] : tmp[x];
-                            diffPtr++;
-                            if (rowPadding > 0)
-                            {
-                                data1Ptr += rowPadding;
-                                data2Ptr += rowPadding;
-                                diffPtr += rowPadding;
-                            }
-                        }
-                        image1.UnlockBits(data1);
-                        image2.UnlockBits(data2);
-                        diffImage.UnlockBits(diffData);
-                       
-                    }
-                }
-            }
-             return diffImage;
         }
     }
 }
