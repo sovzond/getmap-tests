6f9f01ccc3ee7afcfc5131afe0a143074055d43a
pavel <gis5@sovzond.ru>
2016-02-12 14:32:24 +0300

    Тест завершен #582

---
+++ getmap-gui-tests/getmap-gui-tests/TestVectorMenuLayer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System.Drawing;
+
+namespace GetMapTest
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestClass]
+    public class TestVectorMenuLayer
+    {
+        private IWebDriver driver;
+        private const string locationButtonsLayer = "div.svzLayerManagerItem.svzLayerManagerItem1 > div.svzSimpleButton.layerContextMenu";
+        private const string locationButtonsVectorLayer = "div.userLayerMenuContainer.userLayerMenuContainerActive > div.svzSimpleButton";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = Settings.Instance.createDriver();
+            GUI.Login.loginAsGuest(driver, Settings.Instance.BaseUrl);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void CheckVectorMenu()
+        {
+            CheckVectorMenuLayerFakel();
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            GUI.Cleanup.get(driver).Quit();
+        }
+
+        private void CheckVectorMenuLayerFakel()
+        { 
+            GUI.SlideMenu.get(driver).OpenLayers();
+            System.Threading.Thread.Sleep(1000);
+            GUI.Layers.NeftyStructClass.get(driver).OpenCloseList();
+            GUI.Layers.NeftyStructClass.SettingsButtonsClass.get(driver).FakelSettingsButtonClick();
+            try
+            {
+                GUI.Layers.NeftyStructClass.VectorButtonsClass.get(driver).StatisticsLayerClick().ZoomToLayerExtent();
+            }
+            catch(Exception)
+            {
+                Assert.Fail("Над слоем факелы не отобразились две кнопки  - 'Статистика слоя' и  'Приближение к экстенту слоя'.");
+            }
+        }
+    }
+}
