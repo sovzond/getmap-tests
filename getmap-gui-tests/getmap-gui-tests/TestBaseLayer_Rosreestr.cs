bb383ab6c8a6d962b3a5bc19588f0501d612cda6
pavel <gis5@sovzond.ru>
2016-02-16 15:29:21 +0300

    Тест полностью выполнен #547

--- getmap-gui-tests/getmap-gui-tests/TestBaseLayer_Rosreestr.cs
+++ getmap-gui-tests/getmap-gui-tests/TestBaseLayer_Rosreestr.cs
@@ -6,123 +6,124 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
 namespace GetMapTest
 {
+    /// <summary>
+    /// Выполняет проверку на отображение тайлов из верных источников слоев 'Росреестр' и  'OpenStreetMap'.
+    /// </summary>
     [TestClass]
     public class TestBaseLayer_Rosreestr
     {
         private IWebDriver driver;
-        private List<string> listAttributeSrcOpen()
-        {
-            IList<IWebElement> element = driver.FindElements(By.CssSelector("div.olMap img[src*='http://c.tile.openstreetmap.org']"));
-            IList<IWebElement> element1 = driver.FindElements(By.CssSelector("div.olMap img[src*='http://a.tile.openstreetmap.org']"));
-            IList<IWebElement> element2 = driver.FindElements(By.CssSelector("div.olMap img[src*='http://b.tile.openstreetmap.org']"));
-            List<string> listAttributeSrc = new List<string>();
-            foreach (var el in element)
-            {
-                listAttributeSrc.Add(el.GetAttribute("src"));
-            }
-            foreach (var el in element1)
-            {
-                listAttributeSrc.Add(el.GetAttribute("src"));
-            }
-            foreach (var el in element2)
-            {
-                listAttributeSrc.Add(el.GetAttribute("src"));
-            }
-            return listAttributeSrc;
-        }
-        private Boolean AssertAttributeSrcOpen(string ListAttributeSrc)
-        {
-            if (ListAttributeSrc.StartsWith("http://c.tile.openstreetmap.org") != true && ListAttributeSrc.StartsWith("http://a.tile.openstreetmap.org") != true && ListAttributeSrc.StartsWith("http://b.tile.openstreetmap.org") != true)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-        private bool getElementByText(IList<IWebElement> els, string text)
-        {
-            foreach (IWebElement el in els)
-            {
-                if (el.Text == text)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-        private Boolean AssertAttributeSrcRos(string ListAttributeSrc)
-        {
-            if (ListAttributeSrc.StartsWith("http://maps.rosreestr.ru/") != true)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-    private List<string> listAttributeSrcRos()
-        {  
-            IList<IWebElement> element2 = driver.FindElements(By.CssSelector("div.olMap img[src*='http://maps.rosreestr.ru']"));
-            List<string> listAttributeSrc = new List<string>();
-            foreach (var el in element2)
-            {
-                listAttributeSrc.Add(el.GetAttribute("src"));
-            }
-            return listAttributeSrc;
-        }    
-        private void AssertGetElementByText()
-        {
-            IList<IWebElement> el = driver.FindElements(By.ClassName("svzLayerManagerItem"));
-            if (getElementByText(el, "Google") == false) Assert.Fail("не найден Google");
-            if (getElementByText(el, "Росреестр") == false) Assert.Fail("не найден Росреестр");
-            if (getElementByText(el, "OpenStreetMap") == false) Assert.Fail("не найден OpenStreetMap");
-        }              
-        [TestMethod]
-        public void TestRosreestr()
-        {
-            driver = Settings.Instance.createDriver();
-            GUI.Login.loginAsGuest(driver, Settings.Instance.BaseUrl);           
-            driver.FindElement(By.Id("sovzond_widget_SimpleButton_74")).Click();//открывает слои
-            Thread.Sleep(4000);
-            var builder = new Actions(driver);
-            IWebElement element = driver.FindElement(By.Id("sovzond_widget_SimpleButton_0"));         
-            builder.Click(element).Perform(); //открывает базовые слои  
-            AssertGetElementByText();
-            IWebElement element1 = driver.FindElement(By.Id("dijit_form_RadioButton_3"));
-            builder.Click(element1).Perform(); //открывает Росреестр               
-            Thread.Sleep(4000);
-            List<string> ListAttributeSrc = listAttributeSrcRos();
-            for (int n = 0; n < ListAttributeSrc.Count; n++)
-            {
-                if (AssertAttributeSrcRos(ListAttributeSrc[n]) != true)
-                {
-                    Assert.Fail("не показан файл из росреестра ");
-                }
-            }
-        }
-        [TestMethod]
-        public void TestOpenStreetMap()
+        private const string locationLayersInBaseLayers = "svzLayerManagerItem";
+        private IList<IWebElement> listTileA;
+        private IList<IWebElement> listTileB;
+        private IList<IWebElement> listTileC;
+        private IList<IWebElement> listImageRosreestr;
+
+        [TestInitialize]
+        public void Setup()
         {
             driver = Settings.Instance.createDriver();
             GUI.Login.loginAsGuest(driver, Settings.Instance.BaseUrl);
-            driver.FindElement(By.Id("sovzond_widget_SimpleButton_74")).Click();//открывает слои
-            Thread.Sleep(4000);
-            var builder = new Actions(driver);
-            IWebElement element = driver.FindElement(By.Id("sovzond_widget_SimpleButton_0"));
-            builder.Click(element).Perform();//открывает базовые слои  
-            IWebElement element1 = driver.FindElement(By.Id("dijit_form_RadioButton_4"));
-            builder.Click(element1).Perform(); //открывает OpenStreetMap   
-            Thread.Sleep(4000);
+            GUI.SlideMenu.get(driver).OpenLayers().OpenBaseLayers();
+            AssertGetElementByText();
+            listTileA = driver.FindElements(By.CssSelector("div.olMap img[src*='http://a.tile.openstreetmap.org']"));
+            listTileB = driver.FindElements(By.CssSelector("div.olMap img[src*='http://b.tile.openstreetmap.org']"));
+            listTileC = driver.FindElements(By.CssSelector("div.olMap img[src*='http://c.tile.openstreetmap.org']"));
+            listImageRosreestr = driver.FindElements(By.CssSelector("div.olMap img[src*='http://maps.rosreestr.ru']"));
+        }
+
+        /// <summary>
+        /// Выполняет проверку на отображение корректных тайлов из источника 'Росреестр'.
+        /// </summary>
+        [TestMethod]
+        public void TestRosreestr()
+        {
+            GUI.SlideMenu.get(driver).RosreestrClick();
+            List<string> ListAttributeSrc = getListAttributeSrcRos();
+            for (int n = 0; n < ListAttributeSrc.Count; n++)
+            {
+                if (!AssertAttributeSrcRos(ListAttributeSrc[n]))
+                    Assert.Fail("не показан файл из росреестра ");
+            }
+        }
+
+        /// <summary>
+        /// Выполняет проверку на отображение корректных тайлов из источника 'OpenStreetMap'.
+        /// </summary>
+        [TestMethod]
+        public void TestOpenStreetMap()
+        {
+            GUI.SlideMenu.get(driver).OpenStreetMapClick();
             List<string> ListAttributeSrc = listAttributeSrcOpen();
             for (int n = 0; n < ListAttributeSrc.Count; n++)
             {
-                if (AssertAttributeSrcOpen(ListAttributeSrc[n]) != true)
-                {
+                if (!AssertAttributeSrcOpen(ListAttributeSrc[n]))
                     Assert.Fail("не показан файл из openSteetMap");
-                }
+            }
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            GUI.Cleanup.get(driver).Quit();
+        }
+
+        private List<string> AddInListAttributeSrc(IList<IWebElement> elementsForAdd)
+        {
+            List<string> listAttributeSrc = new List<string>();
+            foreach (var el in elementsForAdd)
+                listAttributeSrc.Add(el.GetAttribute("src"));
+            return listAttributeSrc;
+        }
+
+        private List<string> listAttributeSrcOpen()
+        {
+            List<string> listAttributeSrc = new List<string>();
+            listAttributeSrc = AddInListAttributeSrc(listTileC);
+            listAttributeSrc = AddInListAttributeSrc(listTileA);
+            listAttributeSrc = AddInListAttributeSrc(listTileB);
+            return listAttributeSrc;
+        }
+
+        private bool AssertAttributeSrcOpen(string listAttributeSrc)
+        {
+            if (listAttributeSrc.StartsWith("http://c.tile.openstreetmap.org")
+                || listAttributeSrc.StartsWith("http://a.tile.openstreetmap.org")
+                || listAttributeSrc.StartsWith("http://b.tile.openstreetmap.org"))
+                return true;
+            return false;
+        }
+
+        private bool getElementByText(IWebElement el, string text)
+        {
+            if (el.Text == text)
+                return true;
+            return false;
+        }
+
+        private bool AssertAttributeSrcRos(string ListAttributeSrc)
+        {
+            if (ListAttributeSrc.StartsWith("http://maps.rosreestr.ru/"))
+                return true;
+            return false;
+        }
+
+        private List<string> getListAttributeSrcRos()
+        {
+            List<string> listAttributeSrc = new List<string>();
+            foreach (var el in listImageRosreestr)
+                listAttributeSrc.Add(el.GetAttribute("src"));
+            return listAttributeSrc;
+        }
+
+        private void AssertGetElementByText()
+        {
+            IList<IWebElement> listLayersInBaseLayers = driver.FindElements(By.ClassName(locationLayersInBaseLayers));
+            if (listLayersInBaseLayers != null)
+            {
+                Assert.IsFalse(!getElementByText(listLayersInBaseLayers[0], "Google"), "Не найден Google.");
+                Assert.IsFalse(!getElementByText(listLayersInBaseLayers[4], "Росреестр"), "Не найден Росреестр.");
+                Assert.IsFalse(!getElementByText(listLayersInBaseLayers[5], "OpenStreetMap"), "Не найден OpenStreetMap.");
             }
         }
     }
