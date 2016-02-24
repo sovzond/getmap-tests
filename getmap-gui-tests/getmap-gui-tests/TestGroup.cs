c49114bea481dd5a0d13df1c930067f27e625a20
darya <gis5@sovzond.ru>
2016-02-24 15:29:56 +0300

    приближение

--- getmap-gui-tests/getmap-gui-tests/TestGroup.cs
+++ getmap-gui-tests/getmap-gui-tests/TestGroup.cs
@@ -10,6 +10,7 @@ namespace GetMapTest
     {
        private IJavaScriptExecutor js;
         private Utils.TransformJS js1;
+        private IWebDriver driver;
         public class XY
         {
             private int Y;
@@ -35,22 +36,60 @@ namespace GetMapTest
                 return Y;
             }     
         }
-            public string[] getExtent(IJavaScriptExecutor js)
+        public Boolean AssertCenter(Utils.TransformJS js1, IJavaScriptExecutor js, IWebDriver driver)
         {
-            
-            string obl = (string)js.ExecuteScript("return window.portal.stdmap.map.getExtent().toString()");
-            return obl.Split(new Char[] { ',' });
-        }
-        public Boolean AssertCenter(IJavaScriptExecutor js)
-        {
-            double k = Math.Round(((70 + 70.024) / 2), 2);//находим серидину по долготе
-            double k1 = Math.Round(((60.88 + 60.895) / 2), 2); //находим серидину по широте
+            Utils.LonLat startPoint1 = js1.getMapCenter();
+            double StartXL = startPoint1.getLon() - 0.03;
+            double StartYL = startPoint1.getLat() - 0.03;
+            double StartXR = startPoint1.getLon() + 0.03;
+            double StartYR = startPoint1.getLat() + 0.03;
+            GUI.MenuNavigation.get(driver).MagnifyButton();
+            string left = (string)js.ExecuteScript("return window.portal.stdmap.map.getPixelFromLonLat(new OpenLayers.LonLat(" + StartXL + "," + StartYL + ").transform('EPSG:4326','EPSG:3857')).toString()");
+            string right = (string)js.ExecuteScript("return window.portal.stdmap.map.getPixelFromLonLat(new OpenLayers.LonLat(" + StartXR + "," + StartYR + ").transform('EPSG:4326','EPSG:3857')).toString()");
+            XY L = new XY(left);
+            XY R = new XY(right);
+            int XL = L.getX();
+            int YL = L.getY();
+            int XR = R.getX();
+            int YR = R.getY();
+            String[] coord = GUI.GetExtents.get(driver).GetCurrentExtent;//находим экстент после нажатия кнопки приближение обл
+            Utils.LonLat bot = new Utils.LonLat(js1.transferFrom(double.Parse(coord[0]), double.Parse(coord[1]), 900913, 4326));//находим левый нижний угол
+            Utils.LonLat top = new Utils.LonLat(js1.transferFrom(double.Parse(coord[2]), double.Parse(coord[3]), 900913, 4326));//находим правый верхний угол
+            if (StartXL <= bot.getLon() || StartYL <= bot.getLat() || StartXR >= top.getLon() || StartYR >= top.getLat())
+            {
+                Assert.Fail("область не отображается");
+            }
+            var builder = new Actions(driver);
+            IWebElement j = driver.FindElement(By.Id("map"));
+            builder.MoveToElement(j, XL, YL).ClickAndHold().MoveToElement(j, XR, YR).Release().Perform();//рисуется квадрат
+            Thread.Sleep(5000);
+            double Lon = Math.Round(((StartXL + StartXR) / 2), 2);//находим серидину по долготе
+            double Lat = Math.Round(((StartYL + StartYR) / 2), 2); //находим серидину по широте
             Utils.LonLat startPoint = js1.getMapCenter();//находим центр карты
-            double lon = startPoint.getLon();
-            double lat = startPoint.getLat();
-            if (k != lon || k1 != lat)
+            double lonCenter = startPoint.getLon();
+            double latCenter = startPoint.getLat();
+            if (Lon != lonCenter || Lat != latCenter)
             {
               return  false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public Boolean AssertExtent(Utils.TransformJS js1, IWebDriver driver)
+        {
+            String[] coord = GUI.GetExtents.get(driver).GetCurrentExtent;//находим экстент после нажатия кнопки приближение обл
+            Utils.LonLat bot = new Utils.LonLat(js1.transferFrom(double.Parse(coord[0]), double.Parse(coord[1]), 900913, 4326));//находим левый нижний угол
+            Utils.LonLat top = new Utils.LonLat(js1.transferFrom(double.Parse(coord[2]), double.Parse(coord[3]), 900913, 4326));//находим правый верхний угол
+            GUI.ScaleMenu.get(driver).IncrementButton();
+            Thread.Sleep(5000);
+            String[] coordButton = GUI.GetExtents.get(driver).GetCurrentExtent; //находим экстент после нажатия кнопки приближение 
+            Utils.LonLat botBut = new Utils.LonLat(js1.transferFrom(double.Parse(coordButton[0]), double.Parse(coordButton[1]), 900913, 4326));//находим левый нижний угол
+            Utils.LonLat topBut = new Utils.LonLat(js1.transferFrom(double.Parse(coordButton[2]), double.Parse(coordButton[3]), 900913, 4326));//находим правый верхний угол
+            if (bot.getLat() >= botBut.getLat() || bot.getLon() >= botBut.getLon() || top.getLat() <= topBut.getLat() || top.getLon() <= topBut.getLon())
+            {
+                return false;
             }
             else
             {
@@ -60,40 +99,18 @@ namespace GetMapTest
     [TestMethod]
         public void TestMet1()
         {      
-            IWebDriver driver = Settings.Instance.createDriver();
-                       
+            IWebDriver driver = Settings.Instance.createDriver();                    
             IJavaScriptExecutor js = driver as IJavaScriptExecutor;
             Utils.TransformJS js1 = new Utils.TransformJS(driver);
-            GUI.Login.loginAsGuest(driver, Settings.Instance.BaseUrl);
-            GUI.MenuNavigation.get(driver).MagnifyButton();
-            string left = (string)js.ExecuteScript("return window.portal.stdmap.map.getPixelFromLonLat(new OpenLayers.LonLat(70,60.88).transform('EPSG:4326','EPSG:3857')).toString()");
-            string right = (string)js.ExecuteScript("return window.portal.stdmap.map.getPixelFromLonLat(new OpenLayers.LonLat(70.024,60.895).transform('EPSG:4326','EPSG:3857')).toString()");
-            XY L = new XY(left);
-            XY R= new XY(right);                
-            int XL = L.getX();
-            int YL = L.getY();
-            int XR = R.getX(); 
-            int YR = R.getY();
-            var builder = new Actions(driver);
-            IWebElement j = driver.FindElement(By.Id("map"));         
-            builder.MoveToElement(j, XL, YL).ClickAndHold().MoveToElement(j, XR, YR).Release().Perform();//рисуется квадрат
-            Thread.Sleep(5000);
-            double k= Math.Round(((70+ 70.024)/2),2);//находим серидину по долготе
-            double k1 = Math.Round(((60.88 + 60.895) / 2),2); //находим серидину по широте
-            Utils.LonLat startPoint = js1.getMapCenter();//находим центр карты
-            double lon = startPoint.getLon();
-            double lat = startPoint.getLat();
-            AssertCenter(js);
-            if (k != lon || k1!= lat)
+            GUI.Login.loginAsGuest(driver, Settings.Instance.BaseUrl);       
+             if( AssertCenter(js1, js, driver)==false)
             {
-                Assert.Fail("не правильный переход");
+                Assert.Fail("не правильно найден центр и не отображен верный экстент карты");
             }
-            String[] coord = getExtent(js);//находим экстент карты
-            Utils.LonLat lo = new Utils.LonLat(js1.transferFrom(double.Parse(coord[0]), double.Parse(coord[1]), 900913, 4326));//находим левый нижний угол
-            Utils.LonLat lo1 = new Utils.LonLat(js1.transferFrom(double.Parse(coord[2]), double.Parse(coord[3]), 900913, 4326));//находим правый верхний угол
-
-
+           if( AssertExtent( js1, driver)== false)
+            {
+                Assert.Fail(" экстент карты отображается");
+            }
         }
-
     }
 }
